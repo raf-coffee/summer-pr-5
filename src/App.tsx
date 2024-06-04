a4a1e8574e3e6b677e936ae28e25aaffe391514d
raf-coffee <pavel.hexlet@mail.ru>
2024-06-04 17:23:56 +0300

    complete the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,189 @@
-import { useState } from "react";
-import { createPortal } from "react-dom";
-import { completeTodo, deleteTodo } from "./store/todos/todosSlice";
-import { useAppDispatch, useAppSelector } from "./hooks";
-import { Form } from "./Form";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  text: string;
+};
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-  const todos = useAppSelector((state) => state.todos);
-  const dispatch = useAppDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-  const handleDeleteTodo = (id: string) => {
-    dispatch(deleteTodo(id));
-  };
-
-  const handleCompleteTodo = (id: string) => {
-    dispatch(completeTodo(id));
-  };
-
+  const onSubmit: SubmitHandler<FormData> = (data) =>
+    alert(JSON.stringify(data, null, 4));
+  console.log(errors);
   return (
-    <main className="max-w-7xl mx-auto flex flex-col items-center min-h-dvh">
-      <div>
-        <button onClick={() => setShowModal(true)}>Добавить задачу</button>
-        {showModal &&
-          createPortal(
-            <Form showModal={showModal} setShowModal={setShowModal} />,
-            document.body
-          )}
+    <main className="max-w-7xl mx-auto p-4">
+      <div className="max-w-[500px] mx-auto">
+        <form
+          className=" bg-white rounded border border-slate-400 mt-10 p-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="w-full relative">
+            <label
+              htmlFor="firstName"
+              className="block font-medium leading-6 text-gray-900"
+            >
+              Введите имя:
+            </label>
+            <input
+              id="firstName"
+              placeholder="Например: Иван"
+              className={`mt-2 py-2 focus-visible:outline focus-visible:outline-cyan-400 px-3 rounded border border-slate-400 w-full${
+                errors.firstName ? " border-red-600" : ""
+              }`}
+              aria-invalid={errors.firstName ? "true" : "false"}
+              {...register("firstName", {
+                required: "Это обязательное поле",
+                minLength: {
+                  value: 2,
+                  message: "Имя должна быть больше 2 символов",
+                },
+              })}
+            />
+            {errors.firstName && (
+              <span
+                role="alert"
+                className="absolute -bottom-6 left-0 text-red-600"
+              >
+                {errors.firstName.message}
+              </span>
+            )}
+          </div>
+          <div className="w-full mt-6 relative">
+            <label
+              htmlFor="lastName"
+              className="block font-medium leading-6 text-gray-900"
+            >
+              Введите фамилию:
+            </label>
+            <input
+              id="lastName"
+              placeholder="Например: Иванов"
+              className={`mt-2 py-2 focus-visible:outline focus-visible:outline-cyan-400 px-3 rounded border border-slate-400 w-full${
+                errors.lastName ? " border-red-600" : ""
+              }`}
+              aria-invalid={errors.lastName ? "true" : "false"}
+              {...register("lastName", {
+                required: "Это обязательное поле",
+                minLength: {
+                  value: 2,
+                  message: "Фамилия должна быть больше 2 символов",
+                },
+              })}
+            />
+            {errors.lastName && (
+              <span
+                role="alert"
+                className="absolute -bottom-6 left-0 text-red-600"
+              >
+                {errors.lastName.message}
+              </span>
+            )}
+          </div>
+          <div className="w-full mt-6 relative">
+            <label
+              htmlFor="email"
+              className="block font-medium leading-6 text-gray-900"
+            >
+              Введите почту:
+            </label>
+            <input
+              id="email"
+              placeholder="Например: email@example.com"
+              className={`mt-2 py-2 focus-visible:outline focus-visible:outline-cyan-400 px-3 rounded border border-slate-400 w-full${
+                errors.email ? " border-red-600" : ""
+              }`}
+              aria-invalid={errors.email ? "true" : "false"}
+              {...register("email", {
+                required: "Это обязательное поле",
+                pattern: {
+                  value: /^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}$/,
+                  message: "Введите корректный e-mail",
+                },
+              })}
+            />
+            {errors.email && (
+              <span
+                role="alert"
+                className="absolute -bottom-6 left-0 text-red-600"
+              >
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+          <div className="w-full mt-6 relative">
+            <label
+              htmlFor="phoneNumber"
+              className="block font-medium leading-6 text-gray-900"
+            >
+              Введите номер телефона:
+            </label>
+            <input
+              id="phoneNumber"
+              placeholder="Например: 7-111-123-45-67"
+              className={`mt-2 py-2 focus-visible:outline focus-visible:outline-cyan-400 px-3 rounded border border-slate-400 w-full${
+                errors.phoneNumber ? " border-red-600" : ""
+              }`}
+              aria-invalid={errors.phoneNumber ? "true" : "false"}
+              {...register("phoneNumber", {
+                required: "Это обязательное поле",
+                pattern: {
+                  value: /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/,
+                  message: "Введите корректный номер телефона",
+                },
+              })}
+            />
+            {errors.phoneNumber && (
+              <span
+                role="alert"
+                className="absolute -bottom-6 left-0 text-red-600"
+              >
+                {errors.phoneNumber.message}
+              </span>
+            )}
+          </div>
+          <div className="w-full mt-6 relative">
+            <label
+              htmlFor="text"
+              className="block font-medium leading-6 text-gray-900"
+            >
+              Поле ввода текста:
+            </label>
+            <textarea
+              id="text"
+              className={`mt-2 py-2 focus-visible:outline focus-visible:outline-cyan-400 px-3 rounded border border-slate-400 w-full${
+                errors.text ? " border-red-600" : ""
+              }`}
+              aria-invalid={errors.text ? "true" : "false"}
+              {...register("text", {
+                required: "Это обязательное поле",
+                minLength: {
+                  value: 2,
+                  message: "Текст должен быть больше 2 символов",
+                },
+              })}
+            />
+            {errors.text && (
+              <span
+                role="alert"
+                className="absolute -bottom-6 left-0 text-red-600"
+              >
+                {errors.text.message}
+              </span>
+            )}
+          </div>
+          <button className="border bg-blue-600 px-6 py-4 rounded-lg mt-6 font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
+            Отправить
+          </button>
+        </form>
       </div>
-      <h2 className="uppercase mt-10 mb-6">Список активных задач:</h2>
-      <ul
-        role="list"
-        className="divide-y divide-gray-100 bg-white border-2 border-slate-600 p-4 rounded-md min-w-[700px]"
-      >
-        {todos.map((todo) => {
-          if (todo.isActive) {
-            return (
-              <li key={todo.id} className="flex justify-between gap-x-8 py-5">
-                <div
-                  className="min-w-0 flex-auto"
-                  onClick={() => handleCompleteTodo(todo.id)}
-                >
-                  <p className="font-semibold leading-6 text-gray-900">
-                    {todo.title}
-                  </p>
-                  <p className="mt-1 text-sm truncate leading-5 text-gray-500">
-                    {todo.body}
-                  </p>
-                </div>
-                <div className="flex items-center">
-                  <button onClick={() => handleDeleteTodo(todo.id)}>
-                    Delete
-                  </button>
-                </div>
-              </li>
-            );
-          }
-        })}
-      </ul>
-      <h2 className="uppercase mt-12 mb-6">Список выполненных задач:</h2>
-      <ul
-        role="list"
-        className="divide-y divide-gray-100 bg-white border-2 border-slate-600 p-4 rounded-md min-w-[700px]"
-      >
-        {todos.map((todo) => {
-          if (!todo.isActive) {
-            return (
-              <li key={todo.id} className="flex justify-between gap-x-8 py-5">
-                <div
-                  className="min-w-0 flex-auto"
-                  onClick={() => handleCompleteTodo(todo.id)}
-                >
-                  <p className="font-semibold leading-6 text-gray-900">
-                    {todo.title}
-                  </p>
-                  <p className="mt-1 text-sm truncate leading-5 text-gray-500">
-                    {todo.body}
-                  </p>
-                </div>
-                <div className="flex items-center">
-                  <button onClick={() => handleDeleteTodo(todo.id)}>
-                    Delete
-                  </button>
-                </div>
-              </li>
-            );
-          }
-        })}
-      </ul>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./store.ts";
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </StrictMode>
 );
