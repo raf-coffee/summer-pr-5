aca334fae513ada55e1ab31f8fa9d6345c4c2de8
raf-coffee <pavel.hexlet@mail.ru>
2024-06-19 12:19:06 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 
   const onSubmit: SubmitHandler<FormData> = (data) =>
     alert(JSON.stringify(data, null, 4));
-  console.log(errors);
+
   return (
     <main className="max-w-7xl mx-auto p-4">
       <div className="max-w-[500px] mx-auto">
